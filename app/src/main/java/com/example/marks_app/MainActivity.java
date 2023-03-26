2d2a34481b981bb7f87335ca6370253dc71bb5f1
Vikramaditya_Singhai <vikramaditya8408@gmail.com>
2023-03-26 16:05:14 +0530

    sdsdsd

--- app/src/main/java/com/example/marks_app/MainActivity.java
+++ app/src/main/java/com/example/marks_app/MainActivity.java
@@ -9,11 +9,9 @@ import android.view.View;
 import android.widget.*;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
