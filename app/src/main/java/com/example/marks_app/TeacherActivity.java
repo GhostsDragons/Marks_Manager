ac0fdca2f9ff88e1146a8a425e2a89181885517e
Hari Goyal <harigoyal2003@gmail.com>
2023-03-27 16:56:12 +0530

    Tabs hosts

---
+++ app/src/main/java/com/example/marks_app/TeacherActivity.java
@@ -0,0 +1,14 @@
+package com.example.marks_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.*;
+public class TeacherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher);
+    }
+}
